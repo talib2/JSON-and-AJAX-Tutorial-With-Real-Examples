455f5b603bb74502cdae05b6d2edb68b656dd3a6
Talib Abd Alammer <alesawytalib@yahoo.com>
2018-08-29 13:25:36 +0300

    Lesson 1 excess from object

---
+++ js/jsonReq.js
@@ -0,0 +1,7 @@
+var myCat = {
+	"name":"Meowsalot",
+	"species":"cat",
+	"favFood":"tuna"
+	}
+ var excessOpject = document.getElementById("animal-info");
+ excessOpject.innerHTML = myCat.name +"<br>"+myCat.species+"<br>"+myCat.favFood	;
