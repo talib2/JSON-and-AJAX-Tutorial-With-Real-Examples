63b52c13b39b1ba8b782400a49e895bb9ca5d877
Talib Abd Alammer <alesawytalib@yahoo.com>
2018-08-29 15:25:29 +0300

    excess from Arry content two object

--- js/jsonReq.js
+++ js/jsonReq.js
@@ -1,12 +1,18 @@
-var myCat = {
+var thePets =[
+ {
 	"name":"Meowsalot",
 	"species":"cat",
 	"favFood":"tuna"
+	},
+	{
+	"name":"Barky",
+	"species":"dog",
+	"favFood":"carrots"
 	}
+	]
  var excessOpject = document.getElementById("animal-info");
  
  
- /*excess from Array */
- var myFavColors = ["blue","green","purple"];
-excessOpject.innerHTML ="<h1 >axcess from opject </h1>"+ myCat.name +"<br>"+myCat.species+"<br>"+myCat.favFood	+"<hr>"+"<h1> axcess from Array  "+"</h1> "+"<br>"+
-myFavColors[0]+"<br>"+myFavColors[1]+"<br>"+myFavColors[2];
+ /*excess from Array and object */
+ 
+excessOpject.innerHTML ="<h1 >axcess from opject and Array </h1><br>"+thePets[0].name+"<br>" +thePets[0].species+"<br>"+thePets[0].favFood +"<hr>"+thePets[1].name+"<br>" +thePets[1].species+"<br>"+thePets[1].favFood ;
