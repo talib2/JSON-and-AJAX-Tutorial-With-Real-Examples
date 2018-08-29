f09428334fca0a7eef058d8716bbca9489371be7
Talib Abd Alammer <alesawytalib@yahoo.com>
2018-08-29 15:08:25 +0300

    axcess from Array and object

--- js/jsonReq.js
+++ js/jsonReq.js
@@ -4,4 +4,9 @@ var myCat = {
 	"favFood":"tuna"
 	}
  var excessOpject = document.getElementById("animal-info");
- excessOpject.innerHTML = myCat.name +"<br>"+myCat.species+"<br>"+myCat.favFood	;
+ 
+ 
+ /*excess from Array */
+ var myFavColors = ["blue","green","purple"];
+excessOpject.innerHTML ="<h1 >axcess from opject </h1>"+ myCat.name +"<br>"+myCat.species+"<br>"+myCat.favFood	+"<hr>"+"<h1> axcess from Array  "+"</h1> "+"<br>"+
+myFavColors[0]+"<br>"+myFavColors[1]+"<br>"+myFavColors[2];
