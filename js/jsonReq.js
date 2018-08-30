d9ee0c301c4937f1b286d7ac7546905668ced594
Talib Abd Alammer <alesawytalib@yahoo.com>
2018-08-30 09:24:04 +0300

    Requaset AJAX with out function render to axecc addEventListener

--- js/jsonReq.js
+++ js/jsonReq.js
@@ -9,12 +9,15 @@ var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 ourRequest.onload = function() {
 	var ourData = JSON.parse(ourRequest.responseText);
-	console.log(ourData[0]);
+	renderHTML(ourData);
 	};
 	ourRequest.send();
  	
 	});
 
+function renderHTML(data){
+	excessOpject.insertAdjacentHTML('beforeend','Talib Abd Alameer');
+	}
  
  
  
