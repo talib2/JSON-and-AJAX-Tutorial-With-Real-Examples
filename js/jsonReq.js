a083d7c5b1ec132e1b736ceb3a33f0fff3f8ad18
Talib Abd Alammer <alesawytalib@yahoo.com>
2018-08-30 08:53:49 +0300

    AJAX first step

--- js/jsonReq.js
+++ js/jsonReq.js
@@ -4,7 +4,7 @@ var excessOpject = document.getElementById("animal-info");
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 ourRequest.onload = function() {
-	var ourData = ourRequest.responseText;
+	var ourData = JSON.parse(ourRequest.responseText);
 	console.log(ourData[0]);
 	};
 	ourRequest.send();
