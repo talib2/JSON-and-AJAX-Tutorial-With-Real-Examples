0a9ba1063dc9d6577644cb75987f8e9e2b54ec25
Talib Abd Alammer <alesawytalib@yahoo.com>
2018-08-30 08:46:36 +0300

    json Requaset by console first step

--- js/jsonReq.js
+++ js/jsonReq.js
@@ -4,8 +4,9 @@ var excessOpject = document.getElementById("animal-info");
 var ourRequest = new XMLHttpRequest();
 ourRequest.open('GET','https://learnwebcode.github.io/json-example/animals-1.json');
 ourRequest.onload = function() {
-	console.log(ourRequest.responseText);
-	}
+	var ourData = ourRequest.responseText;
+	console.log(ourData[0]);
+	};
 	ourRequest.send();
  
  
